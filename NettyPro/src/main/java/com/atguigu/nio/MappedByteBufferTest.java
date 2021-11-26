585bfcf8494e6ea12ae02511f89833ae82c9a6f5
XuJia <859205795@qq.com>
2021-11-26 19:50:37 +0800

    update

--- NettyPro/src/main/java/com/atguigu/nio/MappedByteBufferTest.java
+++ NettyPro/src/main/java/com/atguigu/nio/MappedByteBufferTest.java
@@ -4,29 +4,31 @@ import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
-/*
-说明
-1. MappedByteBuffer 可让文件直接在内存(堆外内存)修改, 操作系统不需要拷贝一次
+/**
+ * 说明
+ * 1. MappedByteBuffer 可让文件直接在内存(堆外内存)修改, 操作系统不需要拷贝一次，是一种操作系统级别的修改，性能会比较高
  */
+@SuppressWarnings("AlibabaAvoidCommentBehindStatement")
 public class MappedByteBufferTest {
     public static void main(String[] args) throws Exception {
 
         RandomAccessFile randomAccessFile = new RandomAccessFile("1.txt", "rw");
-        //获取对应的通道
+        // 获取对应的通道
         FileChannel channel = randomAccessFile.getChannel();
 
         /**
          * 参数1: FileChannel.MapMode.READ_WRITE 使用的读写模式
          * 参数2： 0 ： 可以直接修改的起始位置
          * 参数3:  5: 是映射到内存的大小(不是索引位置) ,即将 1.txt 的多少个字节映射到内存
-         * 可以直接修改的范围就是 0-5
-         * 实际类型 DirectByteBuffer
+         *           可以直接修改的范围就是 0-5
+         *
+         * 返回的实际类型 DirectByteBuffer
          */
         MappedByteBuffer mappedByteBuffer = channel.map(FileChannel.MapMode.READ_WRITE, 0, 5);
 
         mappedByteBuffer.put(0, (byte) 'H');
         mappedByteBuffer.put(3, (byte) '9');
-        mappedByteBuffer.put(5, (byte) 'Y');//IndexOutOfBoundsException
+        mappedByteBuffer.put(5, (byte) 'Y');    // IndexOutOfBoundsException,因为定义中最多可以修改5个字节
 
         randomAccessFile.close();
         System.out.println("修改成功~~");
