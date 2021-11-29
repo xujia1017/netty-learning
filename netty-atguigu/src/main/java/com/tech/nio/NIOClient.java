63df6d8c30e49ca613f6509a3ded8c9c33bf6974
Athletic <859205795@qq.com>
2021-11-29 20:48:03 +0800

    update

--- netty-atguigu/src/main/java/com/tech/nio/NIOClient.java
+++ netty-atguigu/src/main/java/com/tech/nio/NIOClient.java
@@ -18,10 +18,10 @@ public class NIOClient {
         // 设置非阻塞
         socketChannel.configureBlocking(false);
         // 提供服务器端的ip 和 端口
-        InetSocketAddress inetSocketAddress = new InetSocketAddress("127.0.0.1", 6666);
+        InetSocketAddress serverSocketAddress = new InetSocketAddress("127.0.0.1", 6666);
 
         // 连接服务器
-        if (!socketChannel.connect(inetSocketAddress)) {
+        if (!socketChannel.connect(serverSocketAddress)) {
             while (!socketChannel.finishConnect()) {
                 System.out.println("因为连接需要时间，客户端不会阻塞，可以做其它工作..");
             }
@@ -33,6 +33,7 @@ public class NIOClient {
         ByteBuffer buffer = ByteBuffer.wrap(str.getBytes());
         // 发送数据，将buffer数据写入channel
         socketChannel.write(buffer);
+
         System.in.read();
 
     }
