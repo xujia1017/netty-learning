100d42d15f352ba7fd6b73e04dcbaa8abec980a8
Athletic <859205795@qq.com>
2021-11-29 15:18:30 +0800

    update

--- NettyPro/src/main/java/com/atguigu/nio/NIOServer.java
+++ NettyPro/src/main/java/com/atguigu/nio/NIOServer.java
@@ -2,26 +2,31 @@ package com.atguigu.nio;
 
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.*;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
 
+@SuppressWarnings({"checkstyle:RegexpSingleline", "checkstyle:MagicNumber", "checkstyle:WhitespaceAfter"})
 public class NIOServer {
-    public static void main(String[] args) throws Exception{
 
-        //创建ServerSocketChannel -> ServerSocket
+    public static void main(String[] args) throws Exception {
 
+        // 创建ServerSocketChannel -> ServerSocket
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
 
-        //得到一个Selecor对象
+        // 得到一个Selecor对象
         Selector selector = Selector.open();
 
-        //绑定一个端口6666, 在服务器端监听
+        // 绑定一个端口6666, 在服务器端监听
         serverSocketChannel.socket().bind(new InetSocketAddress(6666));
-        //设置为非阻塞
+
+        // 将ServerSocketChannel设置为非阻塞
         serverSocketChannel.configureBlocking(false);
 
-        //把 serverSocketChannel 注册到  selector 关心 事件为 OP_ACCEPT
+        // 把serverSocketChannel注册到selector, 关心事件为 OP_ACCEPT
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
         System.out.println("注册后的selectionkey 数量=" + selector.keys().size()); // 1
