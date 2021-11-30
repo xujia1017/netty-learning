945585539f11e01834adf0684bf9b1ec6a63a42d
Athletic <859205795@qq.com>
2021-11-30 09:33:48 +0800

    update

--- NettyPro/src/main/java/com/atguigu/nio/zerocopy/NewIOClient.java
+++ NettyPro/src/main/java/com/atguigu/nio/zerocopy/NewIOClient.java
@@ -22,7 +22,7 @@ public class NewIOClient {
         long startTime = System.currentTimeMillis();
 
         //在linux下一个transferTo 方法就可以完成传输
-        //在windows 下 一次调用 transferTo 只能发送8m , 就需要分段传输文件, 而且要主要
+        //在windows 下 一次调用 transferTo 只能发送8m, 就需要分段传输文件, 而且要主要
         //传输时的位置 =》 课后思考...
         //transferTo 底层使用到零拷贝
         long transferCount = fileChannel.transferTo(0, fileChannel.size(), socketChannel);
