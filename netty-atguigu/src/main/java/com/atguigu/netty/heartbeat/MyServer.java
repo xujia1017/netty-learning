44ecf457fd2266d85616b1e9922ab9773f2d228e
859205795@qq.com <859205795@qq.com>
2021-12-07 08:26:22 +0800

    update

--- netty-atguigu/src/main/java/com/atguigu/netty/heartbeat/MyServer.java
+++ netty-atguigu/src/main/java/com/atguigu/netty/heartbeat/MyServer.java
@@ -10,6 +10,7 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.handler.timeout.IdleStateHandler;
 
 import java.util.concurrent.TimeUnit;
@@ -27,27 +28,30 @@ public class MyServer {
 
             serverBootstrap.group(bossGroup, workerGroup);
             serverBootstrap.channel(NioServerSocketChannel.class);
+            // 在bossGroup增加一个日志处理器
             serverBootstrap.handler(new LoggingHandler(LogLevel.INFO));
             serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
 
                 @Override
-                protected void initChannel(SocketChannel ch) throws Exception {
-                    ChannelPipeline pipeline = ch.pipeline();
-                    //加入一个netty 提供 IdleStateHandler
-                    /*
-                    说明
-                    1. IdleStateHandler 是netty 提供的处理空闲状态的处理器
-                    2. long readerIdleTime : 表示多长时间没有读, 就会发送一个心跳检测包检测是否连接
-                    3. long writerIdleTime : 表示多长时间没有写, 就会发送一个心跳检测包检测是否连接
-                    4. long allIdleTime : 表示多长时间没有读写, 就会发送一个心跳检测包检测是否连接
+                protected void initChannel(SocketChannel channel) throws Exception {
+                    ChannelPipeline pipeline = channel.pipeline();
 
-                    5. 文档说明
-                    triggers an {@link IdleStateEvent} when a {@link Channel} has not performed
- * read, write, or both operation for a while.
- *                  6. 当 IdleStateEvent 触发后 , 就会传递给管道 的下一个handler去处理
- *                  通过调用(触发)下一个handler 的 userEventTiggered , 在该方法中去处理 IdleStateEvent(读空闲，写空闲，读写空闲)
+                    /**
+                     *
+                     * 加入一个netty 提供 IdleStateHandler
+                     * 说明
+                     *      1. IdleStateHandler 是netty 提供的处理空闲状态的处理器
+                     *      2. long readerIdleTime : 表示多长时间没有读, 就会发送一个心跳检测包检测是否连接
+                     *      3. long writerIdleTime : 表示多长时间没有写, 就会发送一个心跳检测包检测是否连接
+                     *      4. long allIdleTime : 表示多长时间没有读写, 就会发送一个心跳检测包检测是否连接
+                     *      5. 文档说明
+                     *          triggers an {@link IdleStateEvent
+                    } when a {@link Channel} has not performed read, write, or both operation for a while.
+                     *      6. 当IdleStateEvent触发后, 就会传递给管道的下一个handler去处理，
+                     *         通过调用(触发)下一个handler的userEventTiggered,在该方法中去处理IdleStateEvent(读空闲，写空闲，读写空闲)
                      */
                     pipeline.addLast(new IdleStateHandler(7000,7000,10, TimeUnit.SECONDS));
+
                     //加入一个对空闲检测进一步处理的handler(自定义)
                     pipeline.addLast(new MyServerHandler());
                 }
