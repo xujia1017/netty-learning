d765d2bb87db726a4d4a23767fc7da1c0dc3a8dd
Athletic <859205795@qq.com>
2021-12-16 11:36:36 +0800

    update

--- netty-atguigu/src/main/java/com/atguigu/netty/inboundhandlerandoutboundhandler/MyByteToLongDecoder.java
+++ netty-atguigu/src/main/java/com/atguigu/netty/inboundhandlerandoutboundhandler/MyByteToLongDecoder.java
@@ -9,9 +9,8 @@ import io.netty.handler.codec.ByteToMessageDecoder;
 public class MyByteToLongDecoder extends ByteToMessageDecoder {
 
     /**
-     *
-     * decode 会根据接收的数据，被调用多次, 直到确定没有新的元素被添加到list, 或者是ByteBuf 没有更多的可读字节为止
-     * 如果list out 不为空，就会将list的内容传递给下一个channelinboundhandler处理, 该处理器的方法也会被调用多次
+     * decode会根据接收的数据，被调用多次, 直到确定没有新的元素被添加到list, 或者是ByteBuf 没有更多的可读字节为止。
+     * 如果list out不为空，就会将list的内容传递给下一个 channelinboundhandler 处理, 该处理器的方法也会被调用多次
      *
      * @param ctx 上下文对象
      * @param in 入站的 ByteBuf
--- netty-atguigu/src/main/java/com/atguigu/netty/inboundhandlerandoutboundhandler/MyClientHandler.java
+++ netty-atguigu/src/main/java/com/atguigu/netty/inboundhandlerandoutboundhandler/MyClientHandler.java
@@ -36,11 +36,10 @@ public class MyClientHandler  extends SimpleChannelInboundHandler<Long> {
         //3. MyLongToByteEncoder 父类  MessageToByteEncoder
         //4. 父类  MessageToByteEncoder
         /*
-
          public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
         ByteBuf buf = null;
         try {
-            if (acceptOutboundMessage(msg)) { //判断当前msg 是不是应该处理的类型，如果是就处理，不是就跳过encode
+            if (acceptOutboundMessage(msg)) { //判断当前msg是不是应该处理的类型，如果是就处理，不是就跳过encode
                 @SuppressWarnings("unchecked")
                 I cast = (I) msg;
                 buf = allocateBuffer(ctx, cast, preferDirect);
