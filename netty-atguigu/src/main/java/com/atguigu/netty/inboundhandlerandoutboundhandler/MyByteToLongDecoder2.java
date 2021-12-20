d16d36e8d3c9d08db5b102265453b45cd3a204c7
Athletic <859205795@qq.com>
2021-12-20 23:51:56 +0800

    update

--- netty-atguigu/src/main/java/com/atguigu/netty/inboundhandlerandoutboundhandler/MyByteToLongDecoder2.java
+++ netty-atguigu/src/main/java/com/atguigu/netty/inboundhandlerandoutboundhandler/MyByteToLongDecoder2.java
@@ -10,6 +10,7 @@ import java.util.List;
 /**
  * 通过ReplayingDecoder来改写字节处理，ReplayingDecoder扩展了ByteToMessageDecoder类，
  * 使用这个类，我们不必调用readableBytes()方法。参数S指定了用户状态管理的类型，其中Void代表不需要状态管理
+ *
  * @author xujia
  */
 public class MyByteToLongDecoder2 extends ReplayingDecoder<Void> {
@@ -17,6 +18,7 @@ public class MyByteToLongDecoder2 extends ReplayingDecoder<Void> {
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
 
         System.out.println("MyByteToLongDecoder2 被调用");
+
         //在 ReplayingDecoder 不需要判断数据是否足够读取，内部会进行处理判断
         out.add(in.readLong());
 
