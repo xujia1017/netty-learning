31a8c1781b201629cc45a0596fe1aca1bb540bdb
Athletic <859205795@qq.com>
2021-12-20 22:59:26 +0800

    update

--- netty-atguigu/src/main/java/com/atguigu/netty/inboundhandlerandoutboundhandler/MyServerInitializer.java
+++ netty-atguigu/src/main/java/com/atguigu/netty/inboundhandlerandoutboundhandler/MyServerInitializer.java
@@ -16,10 +16,10 @@ public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
         ChannelPipeline pipeline = ch.pipeline();//一会下断点
 
         //添加对应的handler
-        // 入站的handler进行解码 MyByteToLongDecoder
+        // 入站的handler进行Decoder解码
         //pipeline.addLast(new MyByteToLongDecoder());
         pipeline.addLast(new MyByteToLongDecoder2());
-        //出站的handler进行编码
+        // 出站的handler进行Encoder编码
         pipeline.addLast(new MyLongToByteEncoder());
         //自定义的handler 处理业务逻辑
         pipeline.addLast(new MyServerHandler());
